067b695ebdf174630fd91ac529a7b303c3b35a28
damien <damien@macbook-pro-de-damien.home>
2020-04-06 17:36:53 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    io.emit('create player', 'blablabla')
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
