c74d343fa1c00b3bc66535835264ee579596431c
damien <damien@macbook-pro-de-damien.home>
2020-04-08 18:11:37 +0200

    refactor code app

--- public/app.js
+++ public/app.js
@@ -3,19 +3,17 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 const drawCanvas = () => {
+    ctx.clearRect(0, 0, 600, 600);
     ctx.fillStyle = '#283747';
     ctx.fillRect(0, 0, 600, 600);
 } 
 
 socket.on('send players', function(players) {
     console.log(players)
-    ctx.clearRect(0, 0, 600, 600);
     drawCanvas()
     players.forEach(el => {
-
         ctx.fillStyle = 'red';
         ctx.fillRect(el.x, el.y, el.width, el.width);
-
     })
 })
 
