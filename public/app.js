60b5ea016e4c1ecc79e73a70f41023ac43b66739
damien <damien@macbook-pro-de-damien.home>
2020-04-12 19:05:52 +0200

    add player name

--- public/app.js
+++ public/app.js
@@ -5,6 +5,7 @@ const w = 3000;
 const h = 3000;
 const formStart = document.querySelector('#form-start')
 
+/* Draw Map */
 const drawCanvas = () => {
     canvas.width = w;
     canvas.height = h;
@@ -15,6 +16,7 @@ const drawCanvas = () => {
 
 const dist = {x: 0, y: 0}
 
+/* Start Game */
 formStart.addEventListener('submit', (e) => {
     e.preventDefault();
     let data = {
@@ -26,6 +28,7 @@ formStart.addEventListener('submit', (e) => {
     formStart.style.display = 'none'
 })
 
+/* Draw Game  */
 socket.on('sendPlayers', function(data) {
     let players = data.game.players
     let foods = data.game.foods
@@ -38,6 +41,7 @@ socket.on('sendPlayers', function(data) {
         drawCanvas()
     }
 
+    /* Draw Player  */    
     for (let player in players) {
         let posX = players[player].x
         let posY = players[player].y
@@ -49,8 +53,13 @@ socket.on('sendPlayers', function(data) {
         ctx.lineWidth = 4;
         ctx.strokeStyle = "#F1C40F";
         ctx.stroke();
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.font = "15px Arial";
+        ctx.fillText(players[player].name, posX, posY);
     }
 
+    /* Draw quadrillage  */
     let size = 150;
 
     for (i = 0; i < 20; i++) {
@@ -61,6 +70,7 @@ socket.on('sendPlayers', function(data) {
         }
     }
 
+    /* Draw foods */
     foods.map(food => {
         ctx.beginPath();
         ctx.arc(food[0], food[1], food[2], 0, 2 * Math.PI, false);
@@ -70,6 +80,8 @@ socket.on('sendPlayers', function(data) {
 
 })
 
+
+/* Move player onclick */
 canvas.onclick = (event) => {
     let mousePos = {x: event.offsetX, y: event.offsetY}
 
